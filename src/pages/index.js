dcb981af199edd7aa05b55c7a2bb92a5e1b1074d
Prince Sharma <ps789650@gmail.com>
2023-09-06 23:17:18 +0530

    comitted

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home({data}) {
     <Layout>
       <section className={header}>
         <div>
-          <h2>Design</h2>
+          <h2>Design !</h2>
           <h3>Develop & Deploy</h3>
           <p>Ux Designer & web developer based in manchaster</p>
           <Link className={btn} to="/projects">My Portfolio Projects</Link>
